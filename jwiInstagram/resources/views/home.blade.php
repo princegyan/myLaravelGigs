a8a90ee69420740723f2b19deb692d5b9e16cfc1
princegyan <princealfredgyan@gmail.com>
2019-09-13 12:11:40 +0000

    initial commit

---
+++ jwiInstagram/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+      <div class="col-3 p-5">
+      <img src="/svg/hamburger.png" style="max-height:94px"class="rounded-circle">
+      
+  </div>
+  <div class="col-9 pt-5">
+      <div><h1>myjoyfulway</h1></div>
+      <div class="d-flex">
+        <div class="pr-5"><strong>661 </strong> posts</div>
+        <div class="pr-5"><strong>16,5k </strong> followers</div>
+        <div class="pr-5"><strong>102 </strong> following</div>
+      </div>
+  </div>
+</div>
+</div>
+@endsection
